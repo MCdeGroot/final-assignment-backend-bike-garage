aaf16fd67296ccf1f50f03dbabfcf628fa0af6a5
MCdeGroot <mathijsdegroot@live.nl>
2023-05-10 11:14:08 +0200

    First commit

---
+++ src/test/java/com/example/bikegarage/BikeGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bikegarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeGarageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
