326253caf34e6658ad2f1ad28038d337cd5a2eec
MCdeGroot <mathijsdegroot@live.nl>
2023-07-04 10:05:57 +0200

    add totalHoursDriven to bikeOutputDTo and run tests in Postman

--- src/main/java/com/example/bikegarage/dto/output/BikeOutputDto.java
+++ src/main/java/com/example/bikegarage/dto/output/BikeOutputDto.java
@@ -1,5 +1,6 @@
 package com.example.bikegarage.dto.output;
 
+
 import com.example.bikegarage.model.Part;
 import com.example.bikegarage.model.BikeType;
 import com.example.bikegarage.model.Ride;
@@ -7,7 +8,7 @@ import com.example.bikegarage.model.User;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 
-
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
@@ -18,6 +19,7 @@ public class BikeOutputDto {
     public String model;
     public String name;
     public Double totalDistanceDriven;
+    public Duration totalHoursDriven;
     @Enumerated(EnumType.STRING)
     public BikeType bikeType;
     public Set<Part> bikeParts;
