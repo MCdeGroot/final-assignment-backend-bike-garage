784eb9c4fdbaaf0fbab8a020bf618f5bbb38dda6
MCdeGroot <mathijsdegroot@live.nl>
2023-07-10 21:01:14 +0200

    add getAllRidesByUsername method to RideService

--- src/main/java/com/example/bikegarage/repository/RideRepository.java
+++ src/main/java/com/example/bikegarage/repository/RideRepository.java
@@ -1,6 +1,7 @@
 package com.example.bikegarage.repository;
 
 import com.example.bikegarage.model.Ride;
+import com.example.bikegarage.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,6 @@ import java.util.List;
 public interface RideRepository extends JpaRepository<Ride, Long> {
 
         List<Ride> findByDistanceGreaterThanEqual(Double distance);
-
         List<Ride> findByDateAfterOrDateEquals(LocalDateTime dateStartTime, LocalDateTime dateEndTime);
+        List<Ride> findAllByUser(User user);
         }
