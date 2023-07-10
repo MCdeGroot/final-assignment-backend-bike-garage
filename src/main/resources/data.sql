3ec84ca09be0e3a7191f4b3725ac3a860ad7fd72
MCdeGroot <mathijsdegroot@live.nl>
2023-07-10 15:29:16 +0200

    add username to rides in datasql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,18 +13,18 @@ VALUES (100, 111111111, 'Giant', 'TCR Advanced PRO', 'Purple rain','ROAD', 'math
        (102, 987654321, 'Canyon', 'Lux CFR', 'Mounty', 'MOUNTAIN', 'mathijs');
 
 -- 10 rides met Bike 100
-INSERT INTO rides (id, title_ride, sub_title_ride, distance, date, average_power, normalized_power, time_ride, bike_id)
+INSERT INTO rides (id, title_ride, sub_title_ride, distance, date, average_power, normalized_power, time_ride, bike_id, user_username)
 VALUES
-    (100, 'Namiddagrit', 'Fietsrit', 71.82, '2023-06-21', 250, 254, '7911300000000', 100),
-    (101, 'Namiddagrit', 'Fietsrit', 81.45, '2023-06-19', 273, 298, '8907000000000', 100),
-    (102, 'Pilsjes eruit zweten', 'Fietsrit', 28.36, '2023-06-17', 320, 324, '1067900000000', 100),
-    (103, 'Namiddagrit', 'Fietsrit', 30.11, '2023-06-16', 242, 278, '1816900000000', 100),
-    (104, 'Namiddagrit', 'Fietsrit', 33.46, '2023-06-15', 228, 228, '2004600000000', 100),
-    (105, 'Nachtrit', 'Fietsrit', 33.05, '2023-06-14', 286, 294, '3298000000000', 100),
-    (106, 'Namiddagrit', 'Fietsrit', 38.54, '2023-06-14', 228, 255, '4270000000000' , 100),
-    (107, 'Avondrit', 'Fietsrit', 76.22, '2023-06-12', 270, 277, '7644000000000', 100),
-    (108, 'Klein rondje met papsie', 'Fietsrit', 33.29, '2023-06-11', 207, 239, '3856000000000', 100),
-    (109, 'Ochtendrit', 'Fietsrit', 120.08, '2023-06-10', 223, 272, '4320000000000', 100);
+    (100, 'Namiddagrit', 'Fietsrit', 71.82, '2023-06-21', 250, 254, '7911300000000', 100, 'mathijs'),
+    (101, 'Namiddagrit', 'Fietsrit', 81.45, '2023-06-19', 273, 298, '8907000000000', 100, 'mathijs'),
+    (102, 'Pilsjes eruit zweten', 'Fietsrit', 28.36, '2023-06-17', 320, 324, '1067900000000', 100, 'mathijs'),
+    (103, 'Namiddagrit', 'Fietsrit', 30.11, '2023-06-16', 242, 278, '1816900000000', 100, 'mathijs'),
+    (104, 'Namiddagrit', 'Fietsrit', 33.46, '2023-06-15', 228, 228, '2004600000000', 100, 'mathijs'),
+    (105, 'Nachtrit', 'Fietsrit', 33.05, '2023-06-14', 286, 294, '3298000000000', 100, 'mathijs'),
+    (106, 'Namiddagrit', 'Fietsrit', 38.54, '2023-06-14', 228, 255, '4270000000000' , 100, 'mathijs'),
+    (107, 'Avondrit', 'Fietsrit', 76.22, '2023-06-12', 270, 277, '7644000000000', 100, 'mathijs'),
+    (108, 'Klein rondje met papsie', 'Fietsrit', 33.29, '2023-06-11', 207, 239, '3856000000000', 100, 'mathijs'),
+    (109, 'Ochtendrit', 'Fietsrit', 120.08, '2023-06-10', 223, 272, '4320000000000', 100, 'mathijs');
 
 -- 5 bikeparts Bike 100
 INSERT INTO bike_parts (id, part_type, current_distance_driven, max_distance, bike_id, installation_date)
