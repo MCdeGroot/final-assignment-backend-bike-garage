efb6f1367d1f570a6af01e0b2c31567013cc65f6
MCdeGroot <mathijsdegroot@live.nl>
2023-07-20 09:57:00 +0200

    add Validation rules to BikePartInputDTO and Validation beans to BikePartController

--- src/main/java/com/example/bikegarage/dto/input/PartInputDto.java
+++ src/main/java/com/example/bikegarage/dto/input/PartInputDto.java
@@ -4,13 +4,21 @@ import com.example.bikegarage.model.Bike;
 import com.example.bikegarage.model.PartType;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDateTime;
 
 public class PartInputDto {
-
+    @NotNull
     @Enumerated(EnumType.STRING)
     public PartType partType;
+    @NotNull
+    @Positive
     public Double maxDistance;
+
+    @NotNull
+    @PastOrPresent
     public LocalDateTime installationDate;
 }
