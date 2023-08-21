bb01bd47ad90bcfc40607ed97403326219117e4b
MCdeGroot <mathijsdegroot@live.nl>
2023-08-21 15:29:24 +0200

    add integrationtest to testClass

--- src/main/java/com/example/bikegarage/service/PartService.java
+++ src/main/java/com/example/bikegarage/service/PartService.java
@@ -40,7 +40,7 @@ public class PartService {
         return transferPartModelToPartOutputDto(part);
     }
 
-    public List<PartOutputDto> getAllParts() throws RecordNotFoundException {
+    public static List<PartOutputDto> getAllParts() throws RecordNotFoundException {
         List<PartOutputDto> allPartsOutputDtos = new ArrayList<>();
         List<Part> bikeParts = partRepository.findAll();
 
